033e50d14551c0053efe5434137d25a757fea7de
Radegast12ka <radegast12ka@gmail.com>
2021-02-25 01:05:27 +0100

    initial connect to db commit

---
+++ src/com/company/Database.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database{
+
+    private  Database() {
+
+    }
+
+    public static Connection getDBConnection() {
+        Connection connection = null;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+
+        try{
+            connection = DriverManager.getConnection( "jdbc:mysql://localhost:3306/7swi1","7swi1_user","TajneHeslo123");
+        }catch (SQLException throwable){
+            throwable.printStackTrace();
+        }
+        return connection;
+    }
+
+}
+
